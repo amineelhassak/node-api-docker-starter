32eb665aaffc8e00edf4fbc3dfd3d6aa1d3f8b86
Amine El Hassak <amel-has@e3r1p1.1337.ma>
2024-10-08 05:24:25 +0100

    connect success

--- config/db.js
+++ config/db.js
@@ -4,10 +4,7 @@ const url = 'mongodb://mongo:27017/docker-node-mongo';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(url, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(url);
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
